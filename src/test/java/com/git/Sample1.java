c16df3cf68582e409d5ca885709724c06e4ca055
kalai-1419 <gotokalai19@gmail.com>
2023-08-28 23:43:00 +0530

    third change

--- src/test/java/com/git/Sample1.java
+++ src/test/java/com/git/Sample1.java
@@ -5,6 +5,12 @@ public class Sample1 {
 	public static void main(String[] args) {
 		
 		System.out.println("hello ");
+		System.out.println("hi ");
+		System.out.println("how ");
+		System.out.println("r ");
+		System.out.println("u ");
+		System.out.println("hello ");
+		System.out.println("hello ");
 
 	}
 
